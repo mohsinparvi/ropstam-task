ce1deec82ce2e06a9d1396c589936e99c50fb090
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2025-03-27 23:31:04 +0500

    feat: update header positioning and improve layout consistency

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -10,16 +10,16 @@ const NAV_ITEMS = [
 ];
 const Header = () => {
   return (
-    <header className='mx-auto max-w-screen-xl mt-8 py-4 rounded-2xl bg-black/20 backdrop-blur-sm sticky top-0 z-10'>
-      <div className=' px-4 sm:px-6 lg:px-8'>
-        <div className='flex h-16 items-center justify-between'>
-          <div className='md:flex md:items-center md:gap-12'>
+    <header className="mx-auto max-w-screen-xl  py-4 rounded-2xl bg-black/20 backdrop-blur-sm absolute left-0 right-0 top-8 z-10">
+      <div className=" px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
             <Logo />
           </div>
 
-          <div className='hidden md:block'>
-            <nav aria-label='Global'>
-              <ul className='flex items-center gap-6 text-sm'>
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
                 {NAV_ITEMS.map((item) => (
                   <li key={item.label}>
                     <NavLink href={item.href}>{item.label}</NavLink>
@@ -29,7 +29,7 @@ const Header = () => {
             </nav>
           </div>
           <Button
-            className='bg-primary hover:bg-primary/80 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out'
+            className="bg-primary hover:bg-primary/80 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out"
             placeholder={undefined}
             onPointerEnterCapture={null}
             onPointerLeaveCapture={null}
