b1219a432f072a0e82160ed3d7c6038c55f1d449
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2025-03-28 07:18:50 +0500

    feat: add TestimonialsSection component and integrate it into the Home page layout

--- src/components/pages/home/testimonials-section.tsx
+++ src/components/pages/home/testimonials-section.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Heading from "../../common/heading";
 
 const TestimonialsSection = () => {
   return (
-    <div>TestimonialsSection</div>
-  )
-}
+    <section>
+      <div className="container mx-auto py-10 px-4">
+        <div className="space-y-4">
+          <p className="text-primary text-base font-thin uppercase">
+            Our Client’s
+          </p>
+          <Heading>Testimonials</Heading>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default TestimonialsSection
+export default TestimonialsSection;
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -2,6 +2,7 @@ import GetAppSection from "../components/pages/home/get-app-section";
 import HeroSection from "../components/pages/home/hero-section";
 import SectionOne from "../components/pages/home/section-one";
 import SectionTwo from "../components/pages/home/section-two";
+import TestimonialsSection from "../components/pages/home/testimonials-section";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <SectionOne />
       <SectionTwo />
+      <TestimonialsSection />
       <GetAppSection />
     </>
   );
